7b8807e1aa579fa1877f40f5be095519f490a1ab
hshyune <hyun3164@gmail.com>
2017-09-25 17:47:29 +0900

    9095

---
+++ samsung/9095/src/Main.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1);
+			int n = Integer.parseInt(br.readLine());
+			int[] dp = new int[12];
+			dp[0] = 1;
+			dp[1] = 1;
+			dp[2] = 2;
+			dp[3] = 4;
+
+			for (int i = 4; i < 12; i++) {
+				dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+			}
+			for (int i = 0; i < n; i++) {
+				System.out.println(dp[Integer.parseInt(br.readLine())]);
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
