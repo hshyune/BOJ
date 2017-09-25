db90e6a8bdb3b3121f5046d8824de8c5d7371c03
hshyune <hyun3164@gmail.com>
2017-09-25 17:30:40 +0900

    1107
    런타임에러:
    BufferedReader 에서 채널 0이 인식이 안됨 -> Scanner로 해결

--- samsung/1107/src/Main.java
+++ samsung/1107/src/Main.java
@@ -1,51 +1,50 @@
-import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
+	static int[] btn = null;
+	static int channel = 0;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
-			InputStreamReader in = new InputStreamReader(System.in);
-			BufferedReader br = new BufferedReader(in, 1);
-			int channel = Integer.parseInt(br.readLine());
-			int broken = Integer.parseInt(br.readLine());
-			int[] btn = new int[11];
-			String[] sp = br.readLine().split(" ");
-			for (int i = 0; i < broken; i++) {
-				btn[Integer.parseInt(sp[i])] = 1;
-			}
+		Scanner sc = new Scanner(System.in);
+		channel = sc.nextInt();
+		int broken = sc.nextInt();
 
-			PrintWriter pw = new PrintWriter(System.out);
-			Remote r = new Remote(100, channel, btn);
-			pw.println(r.find());
-			pw.flush();
-			pw.close();
-			br.close();
-		} catch (IOException e) {
-			e.printStackTrace();
+		btn = new int[11];
+		for (int i = 0; i < broken; i++) {
+			btn[sc.nextInt()] = 1;
 		}
+		System.out.println(find());
 	}
 
-}
-
-class Remote {
-	int channel;
-	int cur;
-	int[] broken = null;
-	int cnt = 0;
-
-	public Remote(int cur, int channel, int[] broken) {
-		this.cur = cur;
-		this.channel = channel;
-		this.broken = broken;
+	public static boolean check(int num) {
+		int size = getDigit(num);
+		for (int i = 0; i < size; i++) {
+			if (btn[(num % 10)] == 1) {
+				return false;
+			}
+			num /= 10;
+		}
+		return true;
 	}
 
-	public long find() {
-		long direct = Math.abs(channel - cur);
-		long near = 1000000;
-		long min = 1000000;
-		for (long i = 0; i < 1000001; i++) {
+	public static int getDigit(int num) {
+		int len = 1;
+		while (num != 0) {
+			num = num / 10;
+			if (num == 0) {
+				break;
+			}
+			len++;
+		}
+		return len;
+	}
+
+	public static long find() {
+		int direct = Math.abs(channel - 100);
+		int near = 1000000;
+		for (int i = 0; i < 1000001; i++) {
 			if (check(i)) {
 				if (Math.abs(i - channel) < Math.abs(near - channel)) {
 					near = i;
@@ -57,26 +56,4 @@ class Remote {
 		return Math.min(res, direct);
 	}
 
-	public boolean check(long num) {
-		int size = getDigit(num);
-		for (int i = 0; i < size; i++) {
-			if (broken[(int) (num % 10)] == 1) {
-				return false;
-			}
-			num /= 10;
-		}
-		return true;
-	}
-
-	public int getDigit(long num) {
-		int len = 1;
-		while (num != 0) {
-			num = num / 10;
-			if (num == 0) {
-				break;
-			}
-			len++;
-		}
-		return len;
-	}
 }
