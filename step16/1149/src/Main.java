92684f10b26660b6e6d4f86afb151453d448c387
hshyune <hyun3164@gmail.com>
2017-12-05 16:14:37 +0900

    1149, 1932
    pass

--- step16/1149/src/Main.java
+++ step16/1149/src/Main.java
@@ -1,44 +1,30 @@
-import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class Main {
 
-	static int pre = 4;
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1);
-			int n = Integer.parseInt(br.readLine());
-			long sum = 0;
-			int min = 1001;
-			for (int i = 0; i < n; i++) {
-				String[] sp = br.readLine().split(" ");
-				int[] rgb = new int[3];
-				rgb[0] = Integer.parseInt(sp[0]);
-				rgb[1] = Integer.parseInt(sp[1]);
-				rgb[2] = Integer.parseInt(sp[2]);
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] arr = new int[n + 1][3];
 
-				int tmp = getMin(rgb);
-				sum += tmp;
-			}
-			System.out.println(sum);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static int getMin(int[] arr) {
-		int min = 9999;
-		for (int i = 0; i < 3; i++) {
-			if (i != pre) {
-				if (min >= arr[i]) {
-					min = arr[i];
-					pre = i;
-				}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < 3; j++) {
+				arr[i][j] = sc.nextInt();
 			}
 		}
-		return min;
+
+		int[][] dp = new int[n + 1][3];
+		dp[0][0] = arr[0][0];
+		dp[0][1] = arr[0][1];
+		dp[0][2] = arr[0][2];
+		for (int i = 1; i <= n; i++) {
+			dp[i][0] = Math.min(dp[i - 1][1], dp[i - 1][2]) + arr[i][0];
+			dp[i][1] = Math.min(dp[i - 1][0], dp[i - 1][2]) + arr[i][1];
+			dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][0]) + arr[i][2];
+		}
+		System.out.println(Math.min(Math.min(dp[n][0], dp[n][1]), dp[n][2]));
+		sc.close();
 	}
 
 }
--- step16/1932/src/Main.java
+++ step16/1932/src/Main.java
@@ -1,47 +1,41 @@
 import java.io.*;
+import java.util.Arrays;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		try {
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in), 1);
-			int n = Integer.parseInt(br.readLine());
-			int sum = 0;
-			int pre = 0;
-			for (int i = 1; i <= n; i++) {
-				int[] arr = new int[i];
-				String[] sp = br.readLine().split(" ");
-				for (int j = 0; j < i; j++) {
-					arr[j] = Integer.parseInt(sp[j]);
-				}
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[][] arr = new int[n][n];
+		int[][] dp = new int[n][n];
+		String[] sp = null;
 
-				int[] tmp = getMax(arr, pre);
-				sum += tmp[0];
-				pre = tmp[1];
-				System.out.println(tmp[0] + " " + tmp[1]);
-			}
-			System.out.println(sum);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static int[] getMax(int[] arr, int pre) {
-		int[] res = new int[2];
-		if (arr.length == 1) {
-			res[0] = arr[0];
-			res[1] = 0;
-		} else {
-			if (arr[pre] > arr[pre + 1]) {
-				res[0] = arr[pre];
-				res[1] = pre;
-			} else {
-				res[0] = arr[pre + 1];
-				res[1] = pre + 1;
+		for (int i = 0; i < n; i++) {
+			sp = br.readLine().split(" ");
+			for (int j = 0; j <= i; j++) {
+				arr[i][j] = Integer.parseInt(sp[j]);
 			}
 		}
-		return res;
+
+		dp[0][0] = arr[0][0];
+		if (n >= 2) {
+			dp[1][0] = arr[0][0] + arr[1][0];
+			dp[1][1] = arr[0][0] + arr[1][1];
+		}
+		for (int i = 1; i < n; i++) {
+			dp[i][0] = dp[i - 1][0] + arr[i][0];
+			dp[i][i] = dp[i - 1][i - 1] + arr[i][i];
+			for (int j = 1; j < i; j++) {
+				dp[i][j] = Math.max(dp[i - 1][j - 1] + arr[i][j], dp[i - 1][j] + arr[i][j]);
+			}
+			System.out.println(Arrays.toString(dp[i]));
+		}
+
+		int max = Integer.MIN_VALUE;
+		for (int i = 0; i < n; i++) {
+			max = Math.max(max, dp[n - 1][i]);
+		}
+		System.out.println(max);
 	}
 
 }
